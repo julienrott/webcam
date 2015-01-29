b2690b5f9d2e2fb7078eeefd2829bd8a8bd6af40
Julien ROTT <julien.rott@gmail.com>
2015-01-29 09:45:41 +0100

    reset project

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java $JAVA_OPTS -jar server/webapp-runner-7.0.34.1.jar --port $PORT target/*.war
--- grails-app/conf/ApplicationResources.groovy
+++
@@ -1,6 +0,0 @@
-modules = {
-    application {
-        resource url:'js/twel.js'
-        resource url:'js/application.js'
-    }
-}
